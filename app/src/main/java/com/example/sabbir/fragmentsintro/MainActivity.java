2f2adbc6c28df5f34864ac9f9e4a5883e2af2220
Sabbir <jonyhasan01@gmail.com>
2017-12-21 01:48:25 +0600

    Fragment changing between two buttons.

--- app/src/main/java/com/example/sabbir/fragmentsintro/MainActivity.java
+++ app/src/main/java/com/example/sabbir/fragmentsintro/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.sabbir.fragmentsintro;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void selectFrag(View view) {
+        Fragment fragment;
+
+        if(view == findViewById(R.id.button10))
+            fragment = new FragmentOne();
+        else
+            fragment = new FragmentTwo();
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.fragment5, fragment);
+        transaction.commit();
     }
 }
